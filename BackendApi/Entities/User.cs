d7302d82646b87ed458fa9988fb56b3e1418bce8
Boien <59290906+jonagull@users.noreply.github.com>
2026-02-27 21:16:02 +0100

    fixed init

--- BackendApi/Entities/User.cs
+++ BackendApi/Entities/User.cs
@@ -10,4 +10,5 @@ public class User
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public bool IsActive { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
 }
